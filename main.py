d0918715fdbf9deb5474fcf2563672d2c2188d94
SpyCrabG <banan12007@mail.ru>
2024-04-15 22:18:49 +0500

    window and sound update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PyQt5.QtCore import QCoreApplication, QUrl, QDir
+from PyQt5.QtCore import QCoreApplication, QUrl, QDir, Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5 import uic
@@ -9,17 +9,24 @@ project_path = os.path.abspath(".")  # Путь к папке проекта
 music_dir = os.path.join(project_path, "music")  # Путь к папке с музыкой
 ui_file = os.path.join(project_path, "design", "project_pizdi.ui")  # Путь к UI-файлу
 
+
 def get_music_files():
     music_files = [f for f in os.listdir(music_dir) if f.endswith(".mp3")]
     music_files.sort()  # Сортировка по имени (по номеру трека)
     return music_files
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        # Инициализация плеера
+        self.player = QMediaPlayer()
 
         # Загрузка UI из файла
-        self.ui = uic.loadUi('design\\proect_pizdi.ui', self)
+        self.ui = uic.loadUi("design\\proect_pizdi.ui", self)
+        self.slider.valueChanged[int].connect(self.change_volume)
+        self.slider.setValue(30)
+        self.volume = self.slider.value()
 
         # Инициализация плеера
         self.player = QMediaPlayer()
@@ -33,6 +40,11 @@ class MainWindow(QMainWindow):
 
         # Отображение окна
         self.show()
+        self.player.setVolume(self.volume)
+        self.play_music()
+
+    def change_volume(self, value):
+        self.player.setVolume(value)
 
     def setup_buttons(self):
         # Кнопка "Назад"
@@ -51,13 +63,18 @@ class MainWindow(QMainWindow):
         self.current_index -= 1
         if self.current_index < 0:
             self.current_index = len(self.music_files) - 1
-        self.play_music()
 
     def play_pause(self):
         if self.player.state() == QMediaPlayer.PlayingState:
             self.player.pause()
+            self.ui.btn2.setText(">")
+            self.ui.btn3.setEnabled(False)
+            self.ui.btn1.setEnabled(False)
         else:
             self.player.play()
+            self.ui.btn2.setText("||")
+            self.ui.btn3.setEnabled(True)
+            self.ui.btn1.setEnabled(True)
 
     def next_track(self):
         self.current_index += 1
@@ -72,10 +89,21 @@ class MainWindow(QMainWindow):
         self.player.play()
 
     def open_new_window(self):
-        # ... (Реализация открытия нового окна)
-        pass
+        self.hide()
+        self.win = Window()
+        self.win.show()
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Загрузка UI из файла
+        self.des = uic.loadUi("design\\window2.ui", self)
+        self.setWindowTitle("Электронный тамада")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    sys.exit(app.exec_())
+    app.exec_()
