9c53482e9853774836e5fce0d3e6d4fb95ab3b9e
SpyCrabG <banan12007@mail.ru>
2024-08-20 21:09:12 +0500

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from PyQt5.QtCore import QCoreApplication, QUrl, QDir, Qt, QTimer
+from PyQt5.QtCore import QUrl, QTimer
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout, QMessageBox
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5 import uic, QtTest
